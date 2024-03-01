bd3c7afb096c267129a0c0f01f7f6500bdf8de5f
FungusDungus <102629338+FungusDungus@users.noreply.github.com>
2024-02-29 20:36:30 -0500

    Done???

--- robot3D_basic.py
+++ robot3D_basic.py
@@ -3,6 +3,7 @@
 
 
 from vedo import *
+#import pyautogui
 
 def RotationMatrix(theta, axis_name):
     """ calculate single rotation of $theta$ matrix around x,y or z
@@ -103,112 +104,68 @@ def getLocalFrameMatrix(R_ij, t_ij):
                      [np.zeros((1, 3)),       1]])
     
     return T_ij
-	
+
+def forward_kinematics(Phi, L1, L2, L3, L4):
+    phi_extended = np.array(Phi)
+    phi_extended = np.insert(phi_extended,len(phi_extended) - 1,0)
+    local_frames = np.array([[[3],[2],[0]],[[L1+0.8],[0],[0]],[[L2+0.8],[0],[0]],[[L3+0.4],[0],[0]],[[L4],[0],[0]]])
+    global_frames = [getLocalFrameMatrix(RotationMatrix(phi_extended[0], 'z'), local_frames[0])]
+    for i in range(1,5):
+        current = getLocalFrameMatrix(RotationMatrix(phi_extended[i], 'z'), local_frames[i])
+        global_frames.append(global_frames[i-1] @ current)
+
+    return global_frames[0], global_frames[1], global_frames[2], global_frames[3], [frame[3] for frame in global_frames[4][0:3]]
+
+def forward_kinematics_practical(Phi, L1, L2, L3, L4, radius):
+    phi_extended = np.array(Phi)
+    phi_extended = np.insert(phi_extended,len(phi_extended) - 1,0)
+    local_frames = np.array([[[3],[2],[0]],[[L1+radius*2],[0],[0]],[[L2+radius*2],[0],[0]],[[L3+radius*2],[0],[0]],[[L4],[0],[0]]])
+    global_frames = [getLocalFrameMatrix(RotationMatrix(phi_extended[0], 'z'), local_frames[0])]
+    for i in range(1,5):
+        current = getLocalFrameMatrix(RotationMatrix(phi_extended[i], 'z'), local_frames[i])
+        global_frames.append(global_frames[i-1] @ current)
+
+    return global_frames
+
 
 def main():
+    # Set the limits of the graph x, y, and z ranges 
+    axes = Axes(xrange=(0,20), yrange=(-2,10), zrange=(0,6))
 
-	# Set the limits of the graph x, y, and z ranges 
-	axes = Axes(xrange=(0,20), yrange=(-2,10), zrange=(0,6))
-
-	# Lengths of arm parts 
-	L1 = 5   # Length of link 1
-	L2 = 8   # Length of link 2
-
-	# Joint angles 
-	phi1 = 30     # Rotation angle of part 1 in degrees
-	phi2 = -10    # Rotation angle of part 2 in degrees
-	phi3 = 0      # Rotation angle of the end-effector in degrees
-	
-	# Matrix of Frame 1 (written w.r.t. Frame 0, which is the previous frame) 
-	R_01 = RotationMatrix(phi1, axis_name = 'z')   # Rotation matrix
-	p1   = np.array([[3],[2], [0.0]])              # Frame's origin (w.r.t. previous frame)
-	t_01 = p1                                      # Translation vector
-	
-	T_01 = getLocalFrameMatrix(R_01, t_01)         # Matrix of Frame 1 w.r.t. Frame 0 (i.e., the world frame)
-	
-	# Create the coordinate frame mesh and transform
-	Frame1Arrows = createCoordinateFrameMesh()
-	
-	# Now, let's create a cylinder and add it to the local coordinate frame
-	link1_mesh = Cylinder(r=0.4, 
-	                      height=L1, 
-	                      pos = (L1/2,0,0),
-	                      c="yellow", 
-	                      alpha=.8, 
-	                      axis=(1,0,0)
-	                      )
-	
-	# Also create a sphere to show as an example of a joint
-	r1 = 0.4
-	sphere1 = Sphere(r=r1).pos(-r1,0,0).color("gray").alpha(.8)
-
-	# Combine all parts into a single object 
-	Frame1 = Frame1Arrows + link1_mesh + sphere1
-
-	# Transform the part to position it at its correct location and orientation 
-	Frame1.apply_transform(T_01)  
-	
-
-
-
-
-
-	
-	# Matrix of Frame 2 (written w.r.t. Frame 1, which is the previous frame) 	
-	R_12 = RotationMatrix(phi2, axis_name = 'z')   # Rotation matrix
-	p2   = np.array([[L1],[0.0], [0.0]])           # Frame's origin (w.r.t. previous frame)
-	t_12 = p2                                      # Translation vector
-	
-	# Matrix of Frame 2 w.r.t. Frame 1 
-	T_12 = getLocalFrameMatrix(R_12, t_12)
-	
-	# Matrix of Frame 2 w.r.t. Frame 0 (i.e., the world frame)
-	T_02 = T_01 @ T_12
-	
-	# Create the coordinate frame mesh and transform
-	Frame2Arrows = createCoordinateFrameMesh()
-	
-	# Now, let's create a cylinder and add it to the local coordinate frame
-	link2_mesh = Cylinder(r=0.4, 
-	                      height=L2, 
-	                      pos = (L2/2,0,0),
-	                      c="red", 
-	                      alpha=.8, 
-	                      axis=(1,0,0)
-	                      )
-	
-	# Combine all parts into a single object 
-	Frame2 = Frame2Arrows + link2_mesh
-	
-	# Transform the part to position it at its correct location and orientation 
-	Frame2.apply_transform(T_02)  
-	
-	
-
-	
-	
-	# Matrix of Frame 3 (written w.r.t. Frame 2, which is the previous frame) 	
-	R_23 = RotationMatrix(phi3, axis_name = 'z')   # Rotation matrix
-	p3   = np.array([[L2],[0.0], [0.0]])           # Frame's origin (w.r.t. previous frame)
-	t_23 = p3                                      # Translation vector
-	
-	# Matrix of Frame 3 w.r.t. Frame 2 
-	T_23 = getLocalFrameMatrix(R_23, t_23)
-	
-	# Matrix of Frame 3 w.r.t. Frame 0 (i.e., the world frame)
-	T_03 = T_01 @ T_12 @ T_23
-	
-	# Create the coordinate frame mesh and transform. This point is the end-effector. So, I am 
-	# just creating the coordinate frame. 
-	Frame3 = createCoordinateFrameMesh()
-
-	# Transform the part to position it at its correct location and orientation 
-	Frame3.apply_transform(T_03)  
-
-	# Show everything 
-	show([Frame1, Frame2, Frame3], axes, viewup="z").close()
-	
-
+    # Specifications for each arm & end effector
+    lengths = [8,6,4,0.5]
+    # Note that we will use spheres of radius "radius" as joints
+    radius = 0.4
+    # Animation Loop 
+    # Base of arm is assumed to be fixed at 3,2,0
+    fps = 30
+    duration = 5
+    angles = [0,0,0,0]
+    angle_flags = [1,1,1,1]
+    
+    for k in range(0,fps*duration):
+        for i in range(0,4):
+            test = angles[i] + np.random.rand(1,1) * 5 * angle_flags[i]
+            if test >= 90 or test <= -90:
+                angle_flags[i] *= -1
+                test = angles[i] + np.random.rand(1,1) * 5 * angle_flags[i]
+            angles[i] = test;
+        global_frames = forward_kinematics_practical(angles,lengths[0],lengths[1],lengths[2],lengths[3], radius)
+        
+        # create the components and place them
+        frame1Arrows = createCoordinateFrameMesh()
+        for i in range(0,4):
+            p_s = [frame[3] for frame in global_frames[i][0:3]]
+            c = Cylinder(r = radius, height = lengths[i], pos = (lengths[i]/2 + radius,0,0),c = "black", alpha = 1, axis = (1,0,0))
+            c.apply_transform(global_frames[i])
+            s = Sphere(r = radius).pos(p_s[0],p_s[1],p_s[2]).color("yellow").alpha(.7)
+            frame1Arrows += c + s
+    
+        show(frame1Arrows, axes, viewup="y").close()
+        #image = pyautogui.screenshot(region=(20,50,950,930))
+        #image.save(r"animation\screenshot" + str(k) + ".png")
+        
+        
 
 
 if __name__ == '__main__':
